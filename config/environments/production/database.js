1db628621c6501de0a8b566f0681de549e66b8e1
Patricia Robinson <45735996+patrisms@users.noreply.github.com>
2021-11-08 17:43:30 -0500

    setup postgres config

--- config/environments/production/database.js
+++ config/environments/production/database.js
@@ -1,3 +1,27 @@
+module.exports = {
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'strapi-hook-bookshelf',
+      settings: {
+        client: 'postgres',
+        host: 'ec2-23-23-181-251.compute-1.amazonaws.com',
+        port: 5432,
+        database: 'ddmvg10s931pec',
+        username: 'uoahdqefznrtrd',
+        password: 'f1f5e332f3580fc9c34e95226cf84136884ccbb94e74ec7fd4f23143f2a513ae',
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      },
+      options: {
+        ssl: true,
+      },
+    },
+  },
+};
+
+
 // module.exports = {
 //   defaultConnection: 'default',
 //   connections: {
@@ -18,17 +42,17 @@
 // };
  
 
-module.exports = {
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'mongoose',
-      settings: {
-        uri: 'mongodb+srv://someuser:someuser@cluster0.zspyk.mongodb.net/restaurant?retryWrites=true&w=majority'
-      },
-      options: {
-        ssl: true,
-      },
-    },
-  },
-};
+// module.exports = {
+//   defaultConnection: 'default',
+//   connections: {
+//     default: {
+//       connector: 'mongoose',
+//       settings: {
+//         uri: 'mongodb+srv://someuser:someuser@cluster0.zspyk.mongodb.net/restaurant?retryWrites=true&w=majority'
+//       },
+//       options: {
+//         ssl: true,
+//       },
+//     },
+//   },
+// };

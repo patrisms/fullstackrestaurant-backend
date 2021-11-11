636de203d30277fba9cc911007747e92217af751
Patricia Robinson <45735996+patrisms@users.noreply.github.com>
2021-11-10 21:31:06 -0500

    Adding AWS Upload

--- config/plugins.js
+++ config/plugins.js
@@ -16,3 +16,18 @@ module.exports = {
       },
     },
   };
+  module.exports = ({ env }) => ({
+    upload: {
+      provider: 'aws-s3',
+      providerOptions: {
+        accessKeyId: env('AKIAWQTCOG4ACGFNQSXR'),
+        secretAccessKey: env('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'),
+        region: env('us-east-2'),
+        params: {
+          Bucket: env('patricia-robinsonfullstackrestaurantapp'),
+           StorageClass: env('S3-Standard') // Configure your Amazon S3 Storage Classes (including this to environment variable is not a must)
+        },
+        logger: console // Only if you want to `stdout` logs
+      }
+    }
+   });

b32537c2ba6909812cd730910ff54f0430aff3e6
Patricia Robinson <45735996+patrisms@users.noreply.github.com>
2021-11-11 01:15:51 -0500

    upgrade to latest strapi version and setup cloudinary

--- config/plugins.js
+++ config/plugins.js
@@ -1,18 +1,19 @@
-// module.exports = {
-//     graphql: {
-//         playgroundAlways: true
-//     }
-// }
-
 module.exports = {
-    //
-    graphql: {
-      endpoint: "/graphql",
-      shadowCRUD: true,
-      playgroundAlways: true,
-      depthLimit: 100,
-      apolloServer: {
-        tracing: false,
-      },
+  graphql: {
+    endpoint: "/graphql",
+    shadowCRUD: true,
+    playgroundAlways: true,
+    depthLimit: 100,
+    apolloServer: {
+      tracing: false,
     },
-  };
+  },
+  upload: {
+    provider: "cloudinary",
+    providerOptions: {
+      cloud_name: "dnwdxavgx",
+      api_key: "476438797683372",
+      api_secret: "a1cbYAdvnu5Vn37M7hu9uManluU",
+    },
+  },
+};
